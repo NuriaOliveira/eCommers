dfd5b681db76216ea99aca091cf7810827edd310
Nuria Oliveira <nuriaeo@gmail.com>
2023-07-29 17:41:19 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+
+let mensaje = "";
+class ProductManager{
+    constructor(){
+        this.products = []
+    }
+
+    addProduct (titulo, desc, precio, img, cod, cant) {
+        //no invento de codigo, sino lo ingresan no agrega el producto
+        if(products.find((p)=> p.code === cod) != undefined || cod.trim().length > 0) {
+            producto = new Product();
+            producto.id = products.length == 0 ? 1 : this.newId(products);
+            producto.title = titulo ?? "Sin definir"
+            producto.description = desc ?? "Sin definir"
+            producto.price =  precio ?? 0
+            producto.thumbnail = img ?? "Sin definir"
+            producto.code = cod
+            producto.stock = cant
+
+            products.push(producto)
+        }
+        //en caso de que exista el codido tendría que avisar y no dejar que se agregue
+    }
+
+    getProducts(){
+        return this.products;
+    }
+
+    newId(products){
+        let max = 0;
+        products.foreach((p) => {
+            max = p.id > max? p.id :  max;
+        })
+
+        return max + 1;
+    }
+}
+
+class Product{
+    constructor (){
+        this.id;
+        this.title = "";
+        this.description="";
+        this.price;
+        this.thumbnail;
+        this.code;
+        this.stock;
+    }
+}
+
+
